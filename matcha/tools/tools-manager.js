214204bee4294fc156317bfd5948ab9c54bf611c
Hugues Poncet <hponcet@student.42.fr>
2017-06-21 09:38:00 +0200

    Ajout de la fonction getDate dans les tools

--- matcha/tools/tools-manager.js
+++ matcha/tools/tools-manager.js
@@ -4,5 +4,41 @@ exports.atob = require("atob");
 
 // return a timestamp
 exports.newTimestamp = function () {
-    return Math.round(new Date().getTime() / 1000);
-};
+  return Math.round(new Date().getTime() / 1000)
+}
+
+exports.getDate = () => {
+  const d = new Date();
+  let day = d.getDate()
+  if (day.length === 1) {
+    day = '0' + day
+  }
+  let month = (d.getMonth() + 1)
+  if (month.length === 1) {
+    month = '0' + month
+  }
+  let year = d.getFullYear()
+  let hour = d.getHours()
+  if (hour.length === 1) {
+    hour = '0' + hour
+  }
+  let min = d.getMinutes()
+  if (min.length === 1) {
+    min = '0' + min
+  }
+  let sec = d.getSeconds()
+  if (month.length === 1) {
+    month = '0' + month
+  }
+  let date = day + '/' + month + '/' + year
+  let fullHour = hour + ':' + min + ':' + sec
+
+  function getHour () {
+    return fullHour
+  }
+
+  function getDate () {
+    return date
+  }
+  return date + ' ' + fullHour
+}
