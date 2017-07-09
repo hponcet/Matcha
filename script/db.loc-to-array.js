1a45463ccdeca26439ebe35c656689a8cc7faffd
Ponpon <hugues@lalilo.co>
2017-07-09 20:53:15 +0200

    Modification du script db.loc-to-array.js, creation d'un index 2d

--- script/db.loc-to-array.js
+++ script/db.loc-to-array.js
@@ -1,26 +1,26 @@
-
-
 (function () {
   const _ = require('lodash')
   const MongoClient = require('mongodb').MongoClient
-  const conf = require('../server.conf.js')
+  const conf = require('../matcha/config.js')
 
   MongoClient.connect(conf.db.mongoURI, function (err, db) {
+    if (err) console.log('[error]', err)
     const users = db.collection('users')
     users.find({}, {_id: 1, loc: 1}).toArray(function (err, data) {
-      console.log(data)
+      if (err) console.log('[error]', err)
       data.forEach((loc) => {
-        if (loc.loc) {
+        if (loc.loc && _.isArray(loc.loc) === false) {
           console.log(loc.loc)
           loc.loc = loc.loc.split(', ')
           loc.loc[0] = parseFloat(loc.loc[0])
           loc.loc[1] = parseFloat(loc.loc[1])
-          users.findOneAndUpdate({_id: loc._id}, {$set: {loc:loc.loc}}, function(err, data) {
-          // users.findOne({_id: loc._id}, function(err, data) {
-            console.log(data)
+          users.findOneAndUpdate({_id: loc._id}, {$set: {loc: loc.loc}}, function (err, data) {
+            if (err) console.log('[error]', err)
+            console.log(data.loc)
           })
         }
       })
     })
+    users.createIndex({loc: '2d'})
   })
 })()
